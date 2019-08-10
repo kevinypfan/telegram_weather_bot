955c361f79b879dbbfe8c97f354707729d5bdff0
SITCON2019 <SITCON2019@localhost.localdomain>
2019-08-10 22:46:31 +0800

    Need threading

--- main.py
+++ main.py
@@ -11,18 +11,32 @@ user_location = {}
 #0基隆市 1台北市 2新北市 3桃園縣 4新竹市 5新竹縣 6苗栗縣 7臺中市 8彰化縣 9南投縣 10雲林縣 11嘉義市
 #12嘉義縣 13臺南市 14高雄市 15屏東縣 16臺東縣 17花蓮縣 18宜蘭縣 19澎湖縣 20金門縣 21連江縣 
 
+def get_request(locate,update):
+    request=[1,1,1,1,1]
+    result = weather_api.get_data(locate,request)
+    print(result[1])
+    weather_now = result[0][0]['parameter']['parameterName']
+    weather_index = result[0][0]['parameter']['parameterValue']
+    raining_rate = result[1][0]['parameter']['parameterName']
+    lowest_temp = result[2][0]['parameter']['parameterName']
+    feeling = result[3][0]['parameter']['parameterName']
+    highest_temp = result[4][0]['parameter']['parameterName']
+    update.message.reply_text(locate+"現在的天氣爲"+weather_now+"\n降雨機率: "+raining_rate+"%\n最低溫度: "+lowest_temp+"C 最高溫度: "+highest_temp+"C \n舒適度爲"+feeling)
+    
 def locate_sentence(bot, update):#receive messege
     
     locate = update.message.text.strip()
-    request=[1,1,1,1,1]
+    
     if locate in type_list:
-        result = weather_api.get_data(locate,request)
-        weather_now = result[0]['parameter']['parameterValue']
-        raining_rate = result[1]
+        result = get_request(locate,update)
+    else:
+        update.message.reply_text('靠你確定你說的是臺灣嗎？')
 
 def notification(location,update):
     def hello():
-        update.message.reply_text('Miku is real')
+        update.message.reply_text("個人天氣通知")
+        r=get_request(location,update)
+        
     #call_request
     return hello
 
